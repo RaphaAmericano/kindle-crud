cb2e317a4a9c707bda7ad310f5ddbdafddcb03b8
RaphaAmericano <raphael@raphaelamericano.com.br>
2018-07-22 23:58:26 -0300

    Adicionando bootstrap

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
     <?php 
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
@@ -13,3 +14,4 @@
     <?php require_once 'classes/conexao.class.php'; ?>
 </head>
 <body>
+<div class="container">
--- index.php
+++ index.php
@@ -4,39 +4,44 @@
     include('backend/index.backend.php');    
 ?>
 
+<div class="row">
+    <div class="col">
+        <h2>Cadastrar Usuario</h2>
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+            <label for="nome">Nome</label>
+            <input type="text" name="novo[nome]" class="form-control">
+            <label for="email">Email</label>
+            <input type="email" name="novo[email]" class="form-control">
+            <label for="telefone">Telefone</label>
+            <input type="tel" name="novo[telefone]" maxlength="11" minlength="10" class="form-control">
+            <label for="descricao">Descrição</label>
+            <textarea name="novo[descricao]" class="form-control"></textarea>
+            <input type="submit" value="enviar" class="btn btn-success">
+        </form>    
+    </div>
+</div>
+<div class="row">
+    <div class="col">
+        <h2>Lista de Usuarios</h2>
+        <?php 
+        $usuarios = new Select();
+        $linhas = $usuarios->selecionar_todos('USUARIO');
 
-<h2>Cadastrar Usuario</h2>
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-    <label for="nome">Nome</label>
-    <input type="text" name="novo[nome]">
-    <label for="email">Email</label>
-    <input type="email" name="novo[email]">
-    <label for="telefone">Telefone</label>
-    <input type="tel" name="novo[telefone]" maxlength="11" minlength="10">
-    <label for="descricao">Descrição</label>
-    <textarea name="novo[descricao]"></textarea>
-    <input type="submit" value="enviar">
-</form>
-
-<h2>Lista de Usuarios</h2>
-
-<?php 
-$usuarios = new Select();
-$linhas = $usuarios->selecionar_todos('USUARIO');
-
-if(!empty($linhas)): ?>
-<form action="usuario.php" method="POST">
-    <select name="usuarios">
-    <?php foreach ($linhas as $chave) {
-        print '<option value="'.$chave['ID'].'">'.$chave['NOME'].'</option>';
-    } ?>
-    </select>
-    <input type="submit" value="Selecionar">
-</form>
-<?php else: ?>
-<h3>Não há usuários cadastrados</h3>
-<?php endif; ?>
-
+        if(!empty($linhas)): ?>
+        <form action="usuario.php" method="POST">
+            <select name="usuarios" class="form-control">
+            <?php foreach ($linhas as $chave) {
+                print '<option value="'.$chave['ID'].'">'.$chave['NOME'].'</option>';
+            } ?>
+            </select>
+            <input type="submit" value="Selecionar" class="btn btn-primary">
+        </form>
+        <?php else: ?>
+        <h3>Não há usuários cadastrados</h3>
+        <?php endif; ?>    
+    </div>
+</div>
+</div>
 <script src="js/script.js"></script>
 </body>
 </html>
